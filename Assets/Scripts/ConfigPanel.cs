9fe4d2e620edfbd870b4ea36e464a53edb615735
Douglas Ciole <douglasciole@gmail.com>
2020-02-01 20:04:20 -0800

    Scenes Title and GameOver

---
+++ Assets/Scripts/ConfigPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigPanel : MonoBehaviour
+{
+
+    Animator anim;
+    bool isOpen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void ToggleConfigPanel() {
+        if (anim) {
+            isOpen = !isOpen;
+            anim.SetBool("open", isOpen);
+        }
+    }
+
+}
