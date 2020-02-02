b0911b3e21cde64255a3cf83ebc6e800a6686ca1
Douglas Ciole <douglasciole@gmail.com>
2020-02-01 20:04:20 -0800

    Scenes Title and GameOver

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class LoadLevel : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    public void loadScene()
+    {
+        PlayerPrefs.SetString("stageToLoad", sceneToLoad);
+        SceneManager.LoadScene("loading");
+    }
+}
